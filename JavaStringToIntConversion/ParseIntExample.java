cb9ddb12133fc82722d2ef7427fdefc3a6ac82f7
Greg Zuber <greg@masterofprogramming.com>
2016-09-11 17:25:30 +0200

    Adding JavaStringToIntConversion post

---
+++ JavaStringToIntConversion/ParseIntExample.java
@@ -0,0 +1,9 @@
+package JavaStringToIntConversion;
+
+public class ParseIntExample {
+    public static void main(String[] args) {
+        String string = "5";
+        int integer = Integer.parseInt(string);
+        System.out.println(integer);
+    }
+}
---
+++ JavaStringToIntConversion/ValueOfExample.java
@@ -0,0 +1,12 @@
+package JavaStringToIntConversion;
+
+/**
+ * Created by g on 11.09.16.
+ */
+public class ValueOfExample {
+    public static void main(String[] args) {
+        String string = "5";
+        Integer integer = Integer.valueOf(string);
+        System.out.println(integer);
+    }
+}
